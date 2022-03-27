3e9d61d5d77be3fc886a36857dea0d25141b2402
Nick Chaloult <npchaloult@gmail.com>
2022-03-27 13:08:52 -0400

    Add Cli scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "cloud-storage-job-runner",
+    about = "Download files from a storage bucket in the cloud, run a job on each of them, and upload the results back to the cloud.",
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+)]
+struct Cli {
+    /// Path to config file
+    #[structopt(short, long, parse(from_os_str))]
+    config: PathBuf,
+
+    /// Name of job to run. If not present, runs all jobs specified in the provided config file
+    #[structopt()]
+    job_name: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::from_args();
+    println!("{cli:?}");
 }
