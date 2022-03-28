633c362e6a9c24ff4f0667d566e30c23b952e016
Nick Chaloult <npchaloult@gmail.com>
2022-03-28 18:03:02 -0400

    Add Bucket trait

---
+++ src/bucket.rs
@@ -0,0 +1,30 @@
+use std::{error::Error, path::Path};
+
+pub trait Bucket {
+    /// Downloads the `path_to_remote_inputs` directory, and all its contents,
+    /// from a cloud storage bucket, and saves them on disk at
+    /// `path_to_local_inputs`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `io::Error` if the `path_to_local_inputs` directory doesn't
+    /// exist on disk, and this function fails to create it.
+    fn download_inputs(
+        &self,
+        path_to_remote_inputs: &Path,
+        path_to_local_inputs: &Path,
+    ) -> Result<(), Box<dyn Error>>;
+
+    /// Uploads the `path_to_local_outputs` directory, and all its contents,
+    /// on disk to a cloud storage bucket at `path_to_remote_outputs`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `io::Error` if the `path_to_local_outputs` directory doesn't
+    /// exist on disk.
+    fn upload_outputs(
+        &self,
+        path_to_local_outputs: &Path,
+        path_to_remote_outputs: &Path,
+    ) -> Result<(), Box<dyn Error>>;
+}
