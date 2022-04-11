ba7ecedf70ec42ceee6775a887e90084ab4eb9c0
Nick Chaloult <npchaloult@gmail.com>
2022-04-10 23:22:11 -0400

    Have status() accept a &dyn fmt::Display instead of a &str for the msg

--- src/pretty_print.rs
+++ src/pretty_print.rs
@@ -1,8 +1,14 @@
-use std::io::{self, Write};
+use std::{
+    fmt,
+    io::{self, Write},
+};
 
-use termcolor::{Color::Green, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use termcolor::{
+    Color::{Green, Red},
+    ColorChoice, ColorSpec, StandardStream, WriteColor,
+};
 
-pub fn status(prefix: &str, msg: &str, is_indented: bool) -> io::Result<()> {
+pub fn status(prefix: &str, msg: &dyn fmt::Display, is_indented: bool) -> io::Result<()> {
     let mut stderr = StandardStream::stderr(ColorChoice::Auto);
     stderr.reset()?; // Just in case.
     stderr.set_color(ColorSpec::new().set_bold(true).set_fg(Some(Green)))?;
